d6f768454cad9c54383136f0b8826e6a30b67cfd
Jado <m.mohamed.jad@gmail.com>
2020-05-14 02:30:16 -0400

    added mongoDB and  mutation

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://mostafa:mostafa1@cluster0-xdjy1.mongodb.net/graphql');
+mongoose.connection.once('open',() =>{
+    console.log('database is okay')
+});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
